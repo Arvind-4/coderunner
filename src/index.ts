a310dc2cfbc663eb70a2d548480112b1a99cce47
Arvind <pingarvindforquestions@gmail.com>
2024-03-20 18:32:02 +0000

    Added: files

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,26 @@
 import cors from "cors";
-import Express  from "express";
+import path from "path";
+import express  from "express";
 
-const app = Express();
+const app = express();
 
 app.use(cors());
 
+const buildPath = process.cwd() + "/" + "src" + "/" + "build"
+const assetsPath = process.cwd() + "/" + "src" + "/" + "build" + "/" + "assets";
+const indexfilePath = process.cwd() + "/" + "src" + "/" + "build" + "/" + "index.html";
+
+app.use(express.static(path.resolve(buildPath)));
+app.use(express.static(path.resolve(assetsPath)));
 
 app.get("/api/hello/", (req, res) => {
     res.json({
         message: "AA"
     });
+});
+
+app.use("*", (req, res) => {
+    res.sendFile(path.resolve(indexfilePath));
 });
 
 const PORT = process.env.PORT || 8000;
